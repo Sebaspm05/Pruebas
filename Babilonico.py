cbcc9c87146331183e9e5e54fc8b17b3c8ad8711
Sebaspm05 <sebastian252005pabon@gmail.com>
2023-01-27 11:34:35 -0500

    Babilónico

---
+++ Babilonico.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 27 10:27:33 2023
+
+@author: j.pabonm
+"""
+
+import numpy as np
+import matplotlib.pyplot
+R2= np.identity(2)
+print(R2)
+print(R2.shape)
+print(R2[1][1])
+
